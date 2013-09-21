f52ee6502ef00ee5bbde16b3b88a41708de7d247
jufans <djoephans@gmail.com>
2013-09-21 11:55:32 +0700

    Create index.php
    Data Security System: Simple Encrypt-Decrypt using JavaScript

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Tugas 2</title>
+        <script type="text/javascript" src="skd_tugas2.js"></script>
+    </head>
+    <body>
+        
+        <textarea rows="5" cols="30" placeholder="yg mau di-enkrip" id="enkrip" name="enkrip" onchange="acak()" onkeyup="acak()"/></textarea> encrypt/decrypt
+        <textarea rows="5" cols="30" placeholder="yg mau di-dekrip" id="dekrip" name="dekrip" onchange="tata()" onkeyup="tata()"></textarea>
+        <!--button onclick="test()">Mainkan</button-->
+        <br />
+        <br />
+        <p>Jufans Anurwan I.<br />
+           Copyright(c)2013<br />
+           Under MIT License<br />
+           M3111086 - Sebelas Maret University
+        </p>
+    </body>
+</html>
